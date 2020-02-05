ab26628ab528fd35a3f81c82d052d6b59d433ab0
Jennifer Vargas <jevargasv1990@gmail.com>
2020-02-04 23:40:40 -0500

    Initial commit

---
+++ Challenges/Odd_Occurences_in_Array/solution.js
@@ -0,0 +1,10 @@
+function solution(A) {
+    let result;
+    let array = A.sort(function(a, b) {
+        return (a - b);
+    });
+    
+    for (let i = 0; i < array.length; i+=2) {
+        if ((array[i] !== array[i + 1]) || i + 1 == array.length) return array[i];
+    }
+}
