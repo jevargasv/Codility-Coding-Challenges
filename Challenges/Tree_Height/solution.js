38992b783228a20d3545e5416592176c6a374d8b
Jennifer Vargas <jevargasv1990@gmail.com>
2020-02-05 22:29:10 -0500

    Initial commit

---
+++ Challenges/Tree_Height/solution.js
@@ -0,0 +1,12 @@
+function solution(T) {
+    let l = [];
+    let r = [];
+    
+    if (T == null)
+        return -1;
+        result = 0;
+        result = Math.max(result, 1 + solution(T.l));
+        result = Math.max(result, 1 + solution(T.r));
+    return result;
+    
+}
